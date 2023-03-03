a07baed40d25751777a260eea9f98466e74ffdfd
Mac Deluca <Mac.Deluca@quartech.com>
2023-03-03 10:17:08 -0800

    Updated migration

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,10 +2,6 @@ import supertest from "supertest";
 import { app } from "../server";
 import { PrismaClient } from "@prisma/client";
 
-const request = supertest(app);
-
-const prisma = new PrismaClient();
-
 const PORT = process.env.PORT;
 
 const IS_DEV = process.env.NODE_ENV === "development";
@@ -13,5 +9,9 @@ const IS_DEV = process.env.NODE_ENV === "development";
 const IS_PROD = process.env.NODE_ENV === "production";
 
 const IS_TEST = process.env.NODE_ENV === "test";
+
+const request = supertest(app);
+
+const prisma = new PrismaClient();
 
 export { PORT, IS_DEV, IS_PROD, IS_TEST, request, prisma };
