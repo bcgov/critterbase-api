42ded31802931881f8bf28285c45ef8e2363b3f3
Mac Deluca <Mac.Deluca@quartech.com>
2023-03-07 16:34:37 -0800

    removed bad error throw

--- src/api/critter/critter.router.ts
+++ src/api/critter/critter.router.ts
@@ -12,7 +12,7 @@ export const critterRouter = express.Router();
 critterRouter.get(
   "/",
   catchErrors(async (req: Request, res: Response) => {
-    throw apiError.serverIssue();
+    //throw apiError.serverIssue();
     return res.status(200).json("Critter Router");
   })
 );
