23fe78a8f75e0687feccf571a78f0bb90809598a
JeremyQuartech <jeremy.kissack@quartech.com>
2023-03-08 11:29:47 -0800

    Reomved mock users from service and fixed formatting.

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,82 +1,43 @@
 import { prisma } from "../../utils/constants";
 import type { user } from "@prisma/client";
 
-/**
- ** For testing purposes while database is empty
- */
-const generateMockUserData = (): user[] => {
-  const mockUsers: user[] = [
-    {
-      user_id: "1",
-      system_user_id: "123",
-      system_name: "ABC",
-      keycloak_uuid: "xyz123",
-      create_user: "John",
-      update_user: "Mary",
-      create_timestamp: new Date(),
-      update_timestamp: new Date(),
-    },
-    {
-      user_id: "2",
-      system_user_id: "456",
-      system_name: "DEF",
-      keycloak_uuid: null,
-      create_user: "Bob",
-      update_user: "Alice",
-      create_timestamp: new Date(),
-      update_timestamp: new Date(),
-    },
-    {
-      user_id: "3",
-      system_user_id: "789",
-      system_name: "GHI",
-      keycloak_uuid: "uvw456",
-      create_user: "David",
-      update_user: "Susan",
-      create_timestamp: new Date(),
-      update_timestamp: new Date(),
-    },
-  ];
-
-  return mockUsers;
-};
-
-const createUser = async (data:user): Promise<user> => {
-    const newUser = await prisma.user.create({ data });
-    return newUser;
+const createUser = async (data: user): Promise<user> => {
+  const newUser = await prisma.user.create({ data });
+  return newUser;
 };
 
 const getUsers = async (): Promise<user[]> => {
   const allUsers = await prisma.user.findMany();
-  return allUsers; 
+  return allUsers;
 };
 
 const getUser = async (user_id: string): Promise<user | null> => {
-    const user = await prisma.user.findUnique({
-      where: {
-        user_id: user_id
-      }
-    });
-  return user; 
+  const user = await prisma.user.findUnique({
+    where: {
+      user_id: user_id,
+    },
+  });
+  return user;
 };
 
 const updateUser = async (user_id: string, data: user): Promise<user> => {
-    const updatedUser = await prisma.user.update({
-      where: {
-        user_id: user_id,
-      },
-      data: data,
-    });
-    return updatedUser;
-  };
+  const updatedUser = await prisma.user.update({
+    where: {
+      user_id: user_id,
+    },
+    data: data,
+  });
+  return updatedUser;
+};
 
-  const deleteUser = async (user_id: string): Promise<user> => {
-    const deletedUser = await prisma.user.delete({ //TODO: implement soft-deletion?
-      where: {
-        user_id: user_id,
-      },
-    });
-    return deletedUser;
-  };
+const deleteUser = async (user_id: string): Promise<user> => {
+  const deletedUser = await prisma.user.delete({
+    //TODO: implement soft-deletion?
+    where: {
+      user_id: user_id,
+    },
+  });
+  return deletedUser;
+};
 
 export { createUser, getUsers, getUser, updateUser, deleteUser };
