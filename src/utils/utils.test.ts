250b59676bd385fb30442aa32c0baba4eb62df3d
Mac Deluca <Mac.Deluca@quartech.com>
2023-03-09 10:52:59 -0800

    Updated utils tests

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -5,30 +5,28 @@ import { catchErrors, errorHandler, errorLogger, home } from "./middleware";
 import { apiError } from "./types";
 export const request = supertest(app);
 
-let mockError = {} as apiError;
-let mockRequest = {} as Request;
-let mockNext: NextFunction = jest.fn();
-const mockResponse = {
-  json: jest.fn(),
-  status: jest.fn(() => mockResponse),
-} as unknown as Response;
-
-let server: any;
-
-beforeEach((done) => {
-  server = app.listen(4000, () => {
-    done();
-  });
-  jest.spyOn(console, "error").mockImplementation(() => {});
-  jest.spyOn(console, "log").mockImplementation(() => {});
-});
-
-afterEach((done) => {
-  server && server.close(done);
-});
-
 describe("Utils", () => {
-  describe("File: express_handlers.ts", () => {
+  describe("File: middleware.ts", () => {
+    let mockError = {} as apiError;
+    let mockRequest = {} as Request;
+    let mockNext: NextFunction = jest.fn();
+    const mockResponse = {
+      json: jest.fn(),
+      status: jest.fn(() => mockResponse),
+    } as unknown as Response;
+    let server: any;
+
+    beforeEach((done) => {
+      server = app.listen(4000, () => {
+        done();
+      });
+      jest.spyOn(console, "error").mockImplementation(() => {});
+      jest.spyOn(console, "log").mockImplementation(() => {});
+    });
+
+    afterEach((done) => {
+      server && server.close(done);
+    });
     describe("home()", () => {
       it("sets a json.res", async () => {
         home(mockRequest, mockResponse, mockNext);
@@ -45,7 +43,7 @@ describe("Utils", () => {
     });
 
     describe("errorHandler()", () => {
-      it("Error with message returns status 400 and json", () => {
+      it("apiError with message returns status 400 and json", () => {
         errorHandler(new apiError("test"), mockRequest, mockResponse, mockNext);
         expect(mockResponse.json).toBeCalledWith({
           error: "test",
@@ -53,15 +51,15 @@ describe("Utils", () => {
         expect(mockResponse.status).toBeCalledWith(400);
       });
 
-      it("Error without message returns status 400 and default message", () => {
-        errorHandler(new apiError("test"), mockRequest, mockResponse, mockNext);
+      it("apiError without message returns status 400 and default message", () => {
+        errorHandler(new apiError(), mockRequest, mockResponse, mockNext);
         expect(mockResponse.json).toBeCalledWith({
           error: "Unknown error occurred",
         });
         expect(mockResponse.status).toBeCalledWith(400);
       });
 
-      it("cError returns error status and json", () => {
+      it("apiError returns error status and json", () => {
         errorHandler(
           new apiError("apiError", 555),
           mockRequest,
@@ -72,7 +70,7 @@ describe("Utils", () => {
         expect(mockResponse.status).toBeCalledWith(555);
       });
 
-      it("when cError with no status, returns 400 and json", () => {
+      it("when apiError with no status, returns 400 and json", () => {
         errorHandler(
           new apiError("apiError"),
           mockRequest,
@@ -83,9 +81,9 @@ describe("Utils", () => {
         expect(mockResponse.status).toBeCalledWith(400);
       });
 
-      it("when cError.toString() formats correctly", () => {
-        const e = new apiError("Testing cError", 999);
-        expect(e.toString()).toEqual("error: Testing cError status: 999");
+      it("when apiError.toString() formats correctly", () => {
+        const e = new apiError("Testing apiError", 999);
+        expect(e.toString()).toEqual("error: Testing apiError");
       });
     });
 
