b783dcb166266f27b1fb1c914f88775259cc2374
Graham Stewart <graham.stewart@quartech.com>
2023-03-10 14:03:43 -0800

    initial commit, get critterbyid

--- src/api/critter/critter.service.ts
+++ src/api/critter/critter.service.ts
@@ -5,4 +5,10 @@ const getAllCritters = async (): Promise<critter[]> => {
   return await prisma.critter.findMany();
 };
 
+const getCritterById = async (critter_to_find: string): Promise<critter | null> => {
+  return await prisma.critter.findFirst({
+    where: { critter_id: critter_to_find}
+  })
+}
+
 export { getAllCritters };
