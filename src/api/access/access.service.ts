e587ae2d7fee831c8a72f06c7adc35c315843d68
Graham Stewart <graham.stewart@quartech.com>
2023-04-24 12:24:48 -0700

    updated getTableDataTypes to return enumerated types in an array

--- src/api/access/access.service.ts
+++ src/api/access/access.service.ts
@@ -35,7 +35,15 @@ const loginUser = async (login: LoginCredentials): Promise<user> => {
 };
 
 const getTableDataTypes = async (model: Prisma.ModelName) => {
-  const results = await prisma.$queryRaw`SELECT column_name, data_type FROM information_schema.columns WHERE table_name = ${model}`;
+  const results = await prisma.$queryRaw`
+      WITH enums AS (
+        SELECT typname, array_agg(e.enumlabel) AS enum_vals
+        FROM pg_enum e
+        JOIN pg_type t ON e.enumtypid = t.oid
+        GROUP BY typname)
+      SELECT column_name, udt_name, enum_vals FROM information_schema.columns info 
+      LEFT JOIN enums ON info.column_name = enums.typname
+      WHERE table_name = ${model}`;
   return results;
 }
 
