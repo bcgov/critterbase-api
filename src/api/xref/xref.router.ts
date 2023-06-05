014339471e2b8c3313579e0aef2b00ac8a15cc1d
Mac Deluca <Mac.Deluca@quartech.com>
2023-06-05 13:34:48 -0700

    Updated tests for xref

--- src/api/xref/xref.router.ts
+++ src/api/xref/xref.router.ts
@@ -15,59 +15,63 @@ import {
   xrefTaxonCollectionCategoryFormats,
   xrefTaxonMarkingBodyLocationSchema,
 } from "./xref.utils";
+import { ICbDatabase } from "../../utils/database";
 
-export const xrefRouter = express.Router();
+export const XrefRouter = (db: ICbDatabase) => {
+  const xrefRouter = express.Router();
 
-xrefRouter.get(
-  "/collection-units",
-  catchErrors(async (req: Request, res: Response) => {
-    const { category_id } = CollectionUnitCategoryIdSchema.parse(req.query);
-    if (category_id) {
+  xrefRouter.get(
+    "/collection-units",
+    catchErrors(async (req: Request, res: Response) => {
+      const { category_id } = CollectionUnitCategoryIdSchema.parse(req.query);
+      if (category_id) {
+        const response = await formatParse(
+          getFormat(req),
+          db.getCollectionUnitsFromCategoryId(category_id),
+          xrefCollectionUnitFormats
+        );
+        return res.status(200).json(response);
+      }
+      const { category_name, taxon_name_common, taxon_name_latin } =
+        CollectionUnitCategorySchema.parse(req.query);
       const response = await formatParse(
         getFormat(req),
-        getCollectionUnitsFromCategoryId(category_id),
+        db.getCollectionUnitsFromCategory(
+          category_name,
+          taxon_name_common,
+          taxon_name_latin
+        ),
         xrefCollectionUnitFormats
       );
       return res.status(200).json(response);
-    }
-    const { category_name, taxon_name_common, taxon_name_latin } =
-      CollectionUnitCategorySchema.parse(req.query);
-    const response = await formatParse(
-      getFormat(req),
-      getCollectionUnitsFromCategory(
-        category_name,
-        taxon_name_common,
-        taxon_name_latin
-      ),
-      xrefCollectionUnitFormats
-    );
-    return res.status(200).json(response);
-  })
-);
+    })
+  );
 
-xrefRouter.get(
-  "/taxon-collection-categories",
-  catchErrors(async (req: Request, res: Response) => {
-    const { taxon_id } = taxonIdSchema.parse(req.query);
-    const response = await formatParse(
-      getFormat(req),
-      getTaxonCollectionCategories(taxon_id),
-      xrefTaxonCollectionCategoryFormats
-    );
-    res.status(200).json(response);
-  })
-);
+  xrefRouter.get(
+    "/taxon-collection-categories",
+    catchErrors(async (req: Request, res: Response) => {
+      const { taxon_id } = taxonIdSchema.parse(req.query);
+      const response = await formatParse(
+        getFormat(req),
+        db.getTaxonCollectionCategories(taxon_id),
+        xrefTaxonCollectionCategoryFormats
+      );
+      res.status(200).json(response);
+    })
+  );
 
-xrefRouter.get(
-  "/taxon-marking-body-locations",
-  catchErrors(async (req: Request, res: Response) => {
-    const { taxon_id } = taxonIdSchema.parse(req.query);
-    console.log(taxon_id);
-    const response = await formatParse(
-      getFormat(req),
-      getTaxonMarkingBodyLocations(taxon_id),
-      xrefTaxonMarkingBodyLocationSchema
-    );
-    res.status(200).json(response);
-  })
-);
+  xrefRouter.get(
+    "/taxon-marking-body-locations",
+    catchErrors(async (req: Request, res: Response) => {
+      const { taxon_id } = taxonIdSchema.parse(req.query);
+      const response = await formatParse(
+        getFormat(req),
+        db.getTaxonMarkingBodyLocations(taxon_id),
+        xrefTaxonMarkingBodyLocationSchema
+      );
+      res.status(200).json(response);
+    })
+  );
+
+  return xrefRouter;
+};
