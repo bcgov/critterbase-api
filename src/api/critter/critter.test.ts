ad9578e42b353875355d876be5509f22178af6e6
Graham Stewart <graham.stewart@quartech.com>
2023-06-07 16:59:25 -0700

    Original critter tests updated to succeed with mocks

--- src/api/critter/critter.test.ts
+++ src/api/critter/critter.test.ts
@@ -9,6 +9,8 @@ import {
   getCritterById as _getCritterById,
   getCritterByWlhId as _getCritterByWlhId,
   updateCritter as _updateCritter,
+  getMultipleCrittersByIds as _getMultipleCrittersByIds,
+  getSimilarCritters as _getSimilarCritters,
 } from "./critter.service";
 import { randomUUID } from "crypto";
 import { makeApp } from "../../app";
@@ -16,6 +18,7 @@ import supertest from "supertest";
 import { ICbDatabase } from "../../utils/database";
 import { loginUser } from "../access/access.service";
 import { CritterCreateSchema, CritterDefaultResponse, CritterDefaultResponseSchema, CritterSchema } from "./critter.utils";
+import { apiError } from "../../utils/types";
 
 const getAllCritters = jest.fn();
 const getMultipleCrittersByIds = jest.fn();
@@ -38,7 +41,7 @@ const request = supertest(
     updateCritter,
     createCritter,
     getSimilarCritters,
-    deleteCritter
+    deleteCritter,
   } as Record<keyof ICbDatabase, any>)
 );
 
@@ -48,37 +51,6 @@ const findMany = jest.spyOn(prisma.critter, "findMany").mockImplementation();
 const findUniqueOrThrow = jest.spyOn(prisma.critter, "findUniqueOrThrow").mockImplementation();
 const pDelete = jest.spyOn(prisma.critter, "delete").mockImplementation();
 
-const tempCritters: critter[] = [];
-
-const obtainCritterTemplate =
-  async (): Promise<Prisma.critterUncheckedCreateInput> => {
-    const dummyTaxon = (
-      await prisma.lk_taxon.findFirst({
-        where: {
-          taxon_name_latin: {
-            equals: "Rangifer tarandus",
-            mode: "insensitive",
-          },
-        },
-      })
-    )?.taxon_id;
-    if (!dummyTaxon) throw Error("Could not get taxon for dummy.");
-    return {
-      critter_id: await queryRandomUUID(prisma),
-      sex: "Male",
-      wlh_id: "TEST",
-      taxon_id: dummyTaxon,
-    };
-  };
-
-const createTempCritter = async (): Promise<critter> => {
-  const critter = await obtainCritterTemplate();
-  const res = await prisma.critter.create({
-    data: critter,
-  });
-  tempCritters.push(res);
-  return res;
-};
 
 const CRITTER_ID = '11084b96-5cbd-421e-8106-511ecfb51f7a';
 const WLH_ID = '12-1234';
@@ -97,24 +69,33 @@ const CRITTER: critter = {
 };
 
 const DEFAULTFORMAT_CRITTER = {
-  critter_id: CRITTER_ID,
-  taxon_id: '98f9fede-95fc-4321-9444-7c2742e336fe',
-  wlh_id: WLH_ID,
-  animal_id: 'A13',
-  sex: "Male",
-  responsible_region_nr_id: '4804d622-9539-40e6-a8a5-b7b223c2f09f',
-  create_user: '1af85263-6a7e-4b76-8ca6-118fd3c43f50',
-  update_user: '1af85263-6a7e-4b76-8ca6-118fd3c43f50',
-  create_timestamp: new Date(),
-  update_timestamp: new Date(),
-  critter_comment: 'Hi :)',
+  ...CRITTER,
   lk_taxon: {
     taxon_name_common: 'Caribou',
     taxon_name_latin: 'Rangifer tarandus'
   },
   lk_region_nr: {
     region_nr_name: 'Somewhere'
-  }
+  },
+  critter_collection_unit: [{
+    xref_collection_unit: {
+      lk_collection_category: { category_name: 'name', collection_category_id: '1af85263-6a7e-4b76-8ca6-118fd3c43f50'},
+      unit_name: 'name',
+      unit_description: 'desc'
+    }
+  }],
+  mortality: []
+}
+
+const DETAILEDFORMAT_CRITTER = {
+  ...DEFAULTFORMAT_CRITTER,
+  user_critter_create_userTouser: {
+    system_name: 'CRITTERBASE'
+  },
+  capture: [],
+  marking: [],
+  measurement_qualitative: [],
+  measurement_quantitative: []
 }
 
 beforeAll( async () => {
@@ -133,12 +114,31 @@ beforeEach(() => {
   updateCritter.mockReset();
   createCritter.mockReset();
   getSimilarCritters.mockReset();
+  deleteCritter.mockReset();
 
-  getAllCritters.mockResolvedValue([CRITTER]);
-  getCritterById.mockResolvedValue(CRITTER);
-  getCritterByWlhId.mockResolvedValue(CRITTER);
+ // getAllCritters.mockResolvedValue(Promise.resolve([DEFAULTFORMAT_CRITTER]));
+  getAllCritters.mockImplementation(() => {
+    return [DEFAULTFORMAT_CRITTER];
+  })
+  getCritterById.mockResolvedValue(() => {
+    return DEFAULTFORMAT_CRITTER;
+  });
+  getCritterByWlhId.mockImplementation(() => {
+    return [DEFAULTFORMAT_CRITTER];
+  });
 
-  createCritter.mockResolvedValue(CRITTER);
+  createCritter.mockImplementation(() => {
+    return DEFAULTFORMAT_CRITTER;
+  });
+
+  getCritterByIdWithDetails.mockImplementation(() => {
+    return DEFAULTFORMAT_CRITTER;
+  });
+
+  deleteCritter.mockImplementation(() => {
+    return DEFAULTFORMAT_CRITTER;
+  })
+
 })
 
 describe("API: Critter", () => {
@@ -200,6 +200,7 @@ describe("API: Critter", () => {
         expect(CritterSchema.safeParse(returnedCritter).success).toBe(true);
       })
     });
+  });
   describe("ROUTERS", () => {
     describe("GET /api/critters", () => {
       it("should return status 200", async () => {
@@ -207,34 +208,35 @@ describe("API: Critter", () => {
         const res = await request.get("/api/critters");
         expect(res.status).toBe(200);
         expect(getAllCritters.mock.calls.length).toBe(1);
-        expect(getAllCritters.mock.calls[0][0].critter_id).toBe(CRITTER_ID);
+        expect(getAllCritters.mock.results[0].value[0].critter_id).toBe(CRITTER_ID);
       });
     });
     describe("POST /api/critters/create", () => {
       it("should return a critter with status code 201", async () => {
-        const c = await obtainCritterTemplate();
-        const res = await request.post("/api/critters/create").send(c);
+        const res = await request.post("/api/critters/create").send(CRITTER);
         expect.assertions(2);
         expect(res.status).toBe(201);
-        expect(res.body.wlh_id).toBe("TEST");
+        expect(res.body.wlh_id).toBe(WLH_ID);
       });
       it("should return 400 if trying to create with bad value type", async () => {
-        const c = await obtainCritterTemplate();
         const res = await request
           .post("/api/critters/create")
-          .send({ taxon_id: c.taxon_id, sex: 123 });
+          .send({ taxon_id: '11084b96-5cbd-421e-8106-511ecfb51f7a', sex: 123 });
         expect.assertions(1);
         expect(res.status).toBe(400);
       });
     });
     describe("GET /api/critters?wlh_id={wlh_id}", () => {
       it("should return status 200 and a valid critter", async () => {
-        const res = await request.get("/api/critters?wlh_id=TEST");
+        const res = await request.get("/api/critters?wlh_id=" + WLH_ID);
         expect.assertions(2);
         expect(res.status).toBe(200);
         expect(res.body.length).toBeGreaterThanOrEqual(1);
       });
       it("should return status 404 if searching bad wlh id", async () => {
+        getCritterByWlhId.mockImplementation(() => {
+          return [];
+        })
         const res = await request.get("/api/critters?wlh_id=WHAT");
         expect.assertions(1);
         expect(res.status).toBe(404);
@@ -242,12 +244,14 @@ describe("API: Critter", () => {
     });
     describe("GET /api/critters/:id", () => {
       it("should return status 200", async () => {
-        const c = await createTempCritter();
-        const res = await request.get("/api/critters/" + c.critter_id);
+        const res = await request.get("/api/critters/" + CRITTER_ID);
         expect.assertions(1);
         expect(res.status).toBe(200);
       });
       it("should return status 404 when critter id is not found", async () => {
+        getCritterByIdWithDetails.mockImplementation(() => {
+          throw apiError.notFound('critter_id');
+        })
         const res = await request.get("/api/critters/" + randomUUID());
         expect.assertions(1);
         expect(res.status).toBe(404);
@@ -255,53 +259,51 @@ describe("API: Critter", () => {
     });
     describe("PUT /api/critters/:id", () => {
       it("should return status 200, and update the critter", async () => {
-        const c = await createTempCritter();
+        updateCritter.mockImplementation(() => {
+          return {...DEFAULTFORMAT_CRITTER, animal_id: 'Banana'}
+        })
         const res = await request
-          .put("/api/critters/" + c.critter_id)
+          .put("/api/critters/" + CRITTER_ID)
           .send({ animal_id: "Banana" });
+        console.log(JSON.stringify(res,null,2));
         expect.assertions(2);
         expect(res.status).toBe(200);
         expect(res.body.animal_id).toBe("Banana");
       });
       it("should return status 400 if you try to modify with a bad value type", async () => {
-        const c = await createTempCritter();
+        updateCritter.mockImplementation(() => {
+          throw Error();
+        });
         const res = await request
-          .put("/api/critters/" + c.critter_id)
+          .put("/api/critters/" + CRITTER_ID)
           .send({ sex: 1234 });
         expect.assertions(1);
         expect(res.status).toBe(400);
       });
       it("should return status 404 when critter id is not found", async () => {
-        const res = await request.delete("/api/critters/" + randomUUID());
+        updateCritter.mockImplementation(() => {
+          throw apiError.notFound('critter_id');
+        });
+        const res = await request.put("/api/critters/" + randomUUID()).send({ animal_id: "Banana" });;
         expect.assertions(1);
         expect(res.status).toBe(404);
       });
     });
     describe("DELETE /api/critters/:id", () => {
       it("should return status 200 and delete the critter", async () => {
-        const c = await createTempCritter();
-        const res = await request.delete("/api/critters/" + c.critter_id);
+        const res = await request.delete("/api/critters/" + CRITTER_ID);
         expect.assertions(2);
         expect(res.status).toBe(200);
-        expect(res.body.wlh_id).toBe("TEST");
+        expect(res.body.wlh_id).toBe(WLH_ID);
       });
       it("should return status 404 when critter id is not found", async () => {
+        deleteCritter.mockImplementation(() => {
+          throw apiError.notFound('not found');
+        })
         const res = await request.delete("/api/critters/" + randomUUID());
         expect.assertions(1);
         expect(res.status).toBe(404);
       });
     });
   });
-});
-});
-
-afterAll(async () => {
-  for (const c of tempCritters) {
-    const exists = await prisma.critter.findFirst({
-      where: { critter_id: c.critter_id },
-    });
-    if (exists) {
-      await prisma.critter.delete({ where: { critter_id: c.critter_id } });
-    }
-  }
 });
