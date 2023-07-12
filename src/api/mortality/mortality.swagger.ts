789d2610686ba66e82f691b7125d5a9eaa4e3c34
Graham Stewart <graham.stewart@quartech.com>
2023-07-12 13:59:01 -0700

    Partial mortality doc progress

--- src/api/mortality/mortality.swagger.ts
+++ src/api/mortality/mortality.swagger.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { CommonLocationValidation } from "../location/location.utils";
-import { MortalityIncludeSchema } from "./mortality.utils";
+import { MortalityCreateSchema, MortalityIncludeSchema } from "./mortality.utils";
+import { ZodOpenApiOperationObject } from "zod-openapi";
 
 export const SwaggerMortalityResponseValidation = MortalityIncludeSchema.omit({
     lk_cause_of_death_mortality_proximate_cause_of_death_idTolk_cause_of_death: true, 
@@ -13,4 +14,45 @@ export const SwaggerMortalityResponseValidation = MortalityIncludeSchema.omit({
       ultimate_cause_of_death: z.object({cod_category: z.string(), cod_reason: z.string().nullable()}).nullable(),
       proximate_cause_of_death_taxon: z.object({taxon_id: z.string(), taxon_name_latin: z.string()}).nullable(),
       ultimate_cause_of_death_taxon: z.object({taxon_id: z.string(), taxon_name_latin: z.string()}).nullable()
-  })
+});
+
+const TAG = 'Mortalities';
+
+const getAllMortalities: ZodOpenApiOperationObject = {
+  operationId: 'getAllMortalities',
+    summary: 'Get all mortalities available in the DB.',
+    tags: [TAG],
+    responses: {
+        '200' : {
+            description: 'Returned all mortalities.',
+            content: {
+                'application/json' : {
+                    schema: SwaggerMortalityResponseValidation.array()
+                }
+            }
+        },
+    }
+}
+
+const createMortality: ZodOpenApiOperationObject = {
+  operationId: 'createMortality',
+    summary: 'Create a new mortality.',
+    tags: [TAG],
+    requestBody: {
+      content: {
+        'application/json' : {
+          schema: MortalityCreateSchema
+        }
+      }
+    },
+    responses: {
+        '201' : {
+            description: 'Successfully inserted the mortality.',
+            content: {
+                'application/json' : {
+                    schema: SwaggerMortalityResponseValidation
+                }
+            }
+        },
+    }
+}
