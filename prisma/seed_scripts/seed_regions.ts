0c0c595e788b5b26d5e20ffa9847943798e21388
Mac Deluca <Mac.Deluca@quartech.com>
2024-03-19 12:53:39 -0700

    fix: seed_regions now reads in JSON files with fs to prevent build issue

--- prisma/seed_scripts/seed_regions.ts
+++ prisma/seed_scripts/seed_regions.ts
@@ -1,8 +1,5 @@
-import nrRegionJson from "./region_data/ADM_NR_REGIONS_SP.json";
-import envRegionJson from "./region_data/EADM_WLAP_REGION_BND_AREA_SVW.json";
-import wmuJson from "./region_data/WAA_WILDLIFE_MGMT_UNITS_SVW.json";
-import popUnitJson from "./region_data/GCPB_CARIBOU_POPULATION_SP.json";
 import { prisma } from "../../src/utils/constants";
+import fs from "node:fs/promises";
 
 interface RegionStructure {
   tableName: string;
@@ -14,45 +11,6 @@ interface RegionStructure {
   isMultiPolygon: boolean;
 }
 
-const regionStructureList: RegionStructure[] = [
-  {
-    tableName: "lk_region_nr",
-    tableIdName: "region_nr_id",
-    tableUnitName: "region_nr_name",
-    tableGeomName: "region_geom",
-    importedJson: nrRegionJson,
-    jsonRegionName: "REGION_NAME",
-    isMultiPolygon: false,
-  },
-  {
-    tableName: "lk_region_env",
-    tableIdName: "region_env_id",
-    tableUnitName: "region_env_name",
-    tableGeomName: "region_geom",
-    importedJson: envRegionJson,
-    jsonRegionName: "REGION_NAME",
-    isMultiPolygon: true,
-  },
-  {
-    tableName: "lk_wildlife_management_unit",
-    tableIdName: "wmu_id",
-    tableUnitName: "wmu_name",
-    tableGeomName: "wmu_geom",
-    importedJson: wmuJson,
-    jsonRegionName: "WILDLIFE_MGMT_UNIT_ID",
-    isMultiPolygon: false,
-  },
-  {
-    tableName: "lk_population_unit_temp",
-    tableIdName: "population_unit_id",
-    tableUnitName: "unit_name",
-    tableGeomName: "unit_geom",
-    importedJson: popUnitJson,
-    jsonRegionName: "HERD_NAME",
-    isMultiPolygon: true,
-  },
-];
-
 /**
  * Seeds regions from JSON files.
  * Note: using raw SQL as PostGIS geometry types are not natively supported by prisma.
@@ -61,6 +19,69 @@ const regionStructureList: RegionStructure[] = [
  * @returns {Promise<void>}
  */
 const seedRegions = async () => {
+  let nrRegionJson;
+  let envRegionJson;
+  let wmuJson;
+  let popUnitJson;
+
+  try {
+    /**
+     * Dynamically importing the region files to prevent typescript compilation blowing up.
+     *
+     */
+    nrRegionJson = await fs.readFile("./region_data/ADM_NR_REGIONS_SP.json");
+    envRegionJson = await fs.readFile(
+      "./region_data/EADM_WLAP_REGION_BND_AREA_SVW.json"
+    );
+    wmuJson = await fs.readFile(
+      "./region_data/WAA_WILDLIFE_MGMT_UNITS_SVW.json"
+    );
+    popUnitJson = await fs.readFile(
+      "./region_data/GCPB_CARIBOU_POPULATION_SP.json"
+    );
+  } catch (err) {
+    console.log(`Issue reading JSON region files. ${err}`);
+  }
+
+  const regionStructureList: RegionStructure[] = [
+    {
+      tableName: "lk_region_nr",
+      tableIdName: "region_nr_id",
+      tableUnitName: "region_nr_name",
+      tableGeomName: "region_geom",
+      importedJson: nrRegionJson,
+      jsonRegionName: "REGION_NAME",
+      isMultiPolygon: false,
+    },
+    {
+      tableName: "lk_region_env",
+      tableIdName: "region_env_id",
+      tableUnitName: "region_env_name",
+      tableGeomName: "region_geom",
+      importedJson: envRegionJson,
+      jsonRegionName: "REGION_NAME",
+      isMultiPolygon: true,
+    },
+    {
+      tableName: "lk_wildlife_management_unit",
+      tableIdName: "wmu_id",
+      tableUnitName: "wmu_name",
+      tableGeomName: "wmu_geom",
+      importedJson: wmuJson,
+      jsonRegionName: "WILDLIFE_MGMT_UNIT_ID",
+      isMultiPolygon: false,
+    },
+    {
+      tableName: "lk_population_unit_temp",
+      tableIdName: "population_unit_id",
+      tableUnitName: "unit_name",
+      tableGeomName: "unit_geom",
+      importedJson: popUnitJson,
+      jsonRegionName: "HERD_NAME",
+      isMultiPolygon: true,
+    },
+  ];
+
   console.log(`Seeding (${regionStructureList.length}) region JSON files...`);
 
   /**
