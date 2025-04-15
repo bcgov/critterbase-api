d65ee54de42e9c1c3b248c207d1815275e0f586f
Andrew Thompson <andrew.thompson@gov.bc.ca>
2025-04-15 13:35:12 -0700

    initial commit, added Accident

---
+++ prisma/migrations/20250415102400_mortality_update/migration.sql
@@ -0,0 +1,6 @@
+----------------------------------------------------------------------------------------
+-- New cause of death type "Accident"
+----------------------------------------------------------------------------------------
+
+INSERT INTO lk_cause_of_death (cod_category, cod_reason)
+VALUES ('Accident', 'Capture Accident');
